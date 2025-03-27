d538c798164804ac8fe82a020dc93869783d06e6
ElangoManikandan <108584490+ElangoManikandan@users.noreply.github.com>
2025-03-27 10:08:46 +0530

    Update AgendaParoki.jsx

--- src/app/sections/AgendaParoki.jsx
+++ src/app/sections/AgendaParoki.jsx
@@ -24,7 +24,7 @@ const AgendaParokiSection = () => {
     <div className="w-full flex justify-center  bg-white-secondary">
       <div className="w-[95%] max-w-[1300px]">
         <div className="flex flex-col items-center mb-16">
-          <h1 className="font-heading text-[2rem] text-center min-w-max text-blue-primary"><b>Artikel</b> Terbaru</h1>
+          <h1 className="font-heading text-[2rem] text-center min-w-max text-blue-primary"><b>Kegiatan</b> Paroki</h1>
           <div className="w-8 h-1.5 bg-blue-primary" />
           <div className="grid grid-cols-2 py-2 gap-2 mx-2 sm:grid-cols-3 sm:py-3 sm:gap-3 sm:mx-3 md:grid-cols-3 md:py-6 md:gap-4 md:mx-3 lg:grid-cols-4 lg:py-8 lg:gap-6 lg:mx-4">
           {berita.map((item) => (
